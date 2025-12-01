213096f3f2efde88ee374ebcf5e30b1e710e4bfc
Khalilou5 <khalilouibra@gmail.com>
2025-12-01 10:32:20 +0000

    message

--- components/PlanUnifilairePiloteSolis.tsx
+++ components/PlanUnifilairePiloteSolis.tsx
@@ -901,7 +901,7 @@ const PlanUnifilairePiloteSolis: React.FC = () => {
               <g id="niveau-tgbt">
                 {/* TGBT Block avec Bus Bar */}
                 {renderComponentBlock('tgbt', 750, 180, 180, 280, 210, 235, 320)}
-                <text x="837" y="415" textAnchor="middle" fill="#10b981" fontSize="9" fontWeight="bold">Icw: 30 kA | XCBR</text>
+                <text x="837" y="255" textAnchor="middle" fill="#10b981" fontSize="9" fontWeight="bold">Icw: 30 kA | XCBR</text>
 
                 {/* 1. Onduleurs -> TGBT Bus (Ligne déjà dessinée au dessus) */}
 
